c6cab3b6677fb7b48976289d1d016f405d682ecd
porkchop38 <porkchop3838@gmail.com>
2019-10-26 10:24:09 +0100

    Optimised Effect Functions

--- effects.py
+++ effects.py
@@ -1,26 +1,17 @@
 def fade_in(sprite, speed):
-	# increase alpha until max is reached
-	for i in range(speed):
-		if sprite.alpha < 255:
-			sprite.alpha += 1
-		else:
-			sprite.alpha = 255
-			break
+    # increase alpha until max is reached
+    sprite.alpha += speed
+    if sprite.alpha > 255:
+        sprite.alpha = 255
 
 def fade_out(sprite, speed):
-	# decrease alpha until min is reached
-	for i in range(speed):
-		if sprite.alpha > 0:
-			sprite.alpha -= 1
-		else:
-			sprite.alpha = 0
-			break
+    # decrease alpha until min is reached
+    sprite.alpha -= speed
+    if sprite.alpha < 0:
+        sprite.alpha = 0
 
 def slide_in(sprite, speed, finish_height):
-	# increase center_y until finish_height is reached
-	for i in range(speed):
-		if sprite.center_y < finish_height:
-			sprite.center_y += 1
-		else:
-			sprite.center_y = finish_height
-			break
+    # increase center_y until finish_height is reached
+        sprite.center_y += speed
+        if sprite.center_y > finish_height:
+        	sprite.center_y = finish_height
