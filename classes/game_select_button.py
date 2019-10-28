102420803ed59b2686fc9f3cfb8fd9d2ba2da381
Alex Gale <alexgaleiii@gmail.com>
2019-10-28 10:45:29 +0000

    Added spacing between lines of desc_text
    * Added 2px space between each line of GameSelectButton description text
    * Decreased the line_width of the title underline to 1px

--- classes/game_select_button.py
+++ classes/game_select_button.py
@@ -29,17 +29,16 @@ class GameSelectButton:
         # create other shapes
         self.border_line = arcade.create_line(self.x, self.y - 152, self.x + (self.width), self.y - 152, [140, 140, 140, self.alpha], line_width=4)
         self.text_rectangle = arcade.create_rectangle_filled(self.x + (self.width / 2), self.y - 277, 200, 246, [184, 184, 184, self.alpha])
-        self.title_underline = arcade.create_line(self.x + 10, self.y - 185, self.x + (self.width - 10), self.y - 185, [0, 0, 0, self.alpha], line_width=2)
+        self.title_underline = arcade.create_line(self.x + 10, self.y - 185, self.x + (self.width - 10), self.y - 185, [0, 0, 0, self.alpha])
         self.button_border = arcade.create_rectangle_outline(self.center_x, self.center_y, self.width - 4, self.height - 4, [140, 140, 140, self.alpha], 4)
 
         # initialise text values
         self.title_text = title_text
         self.desc_test = desc_text
 
-        # fit desc_text into the space given with line breaks
+        # wrap desc_text to fit in the box
         wrapper = textwrap.TextWrapper(width=25)
-        self.desc_test_lines = wrapper.fill(self.desc_test)
-        print(self.desc_test_lines)
+        self.desc_text_lines = wrapper.wrap(self.desc_test)
 
         self.pressed = False
 
@@ -51,9 +50,14 @@ class GameSelectButton:
         self.title_underline.draw()
         self.button_border.draw()
 
-        # draw text
+        # draw titletext
         arcade.draw_text(self.title_text, self.x + 10, self.y - 180, [0, 0, 0, self.alpha], 20, font_name="fonts/Courgette-Regular.ttf")
-        arcade.draw_text(self.desc_test_lines, self.x + 10, self.y - 195, [0, 0, 0, self.alpha], 13, anchor_x="left", anchor_y="top")
+
+        # draw each line of description text
+        line_offset = 0
+        for line in self.desc_text_lines:
+            arcade.draw_text(line, self.x + 10, self.y - 195 - line_offset, [0, 0, 0, self.alpha], 13, anchor_x="left", anchor_y="top")
+            line_offset += 15
 
     def on_press(self):
         self.pressed = True
