ebbfb1b70852d0e7aa800be283a634f560a4be52
porkchop38 <porkchop3838@gmail.com>
2019-10-29 18:58:33 +0000

    Added update position to BorderRadiusRectangle
    * Added ability to update the position of BorderRadiusRectangles

--- classes/shapes.py
+++ classes/shapes.py
@@ -1,7 +1,7 @@
 import arcade
 
 class BorderRadiusRectangle:
-    def __init__(self, x=0, y=0, width=0, height=0, colour=(255,255,255), border_rad=0, resolution=1):
+    def __init__(self, x=0, y=0, width=100, height=100, colour=(255,255,255), border_rad=0, resolution=1):
 
         self.center_x = x
         self.center_y = y
@@ -13,6 +13,9 @@ class BorderRadiusRectangle:
         self.border_rad = border_rad
         self.res = resolution
 
+        self.set_points()
+
+    def set_points(self):
         # define points on the rectangle
         self.bottom_l_1 = (self.center_x - (self.width // 2), self.center_y - (self.height // 2) + self.border_rad)
         self.top_l_1 = (self.center_x - (self.width // 2), self.center_y + (self.height // 2) - self.border_rad)
@@ -22,6 +25,11 @@ class BorderRadiusRectangle:
         self.bottom_r_1 = (self.center_x + (self.width // 2), self.center_y - (self.height // 2) + self.border_rad)
         self.bottom_r_2 = (self.center_x + (self.width // 2) - self.border_rad, self.center_y - (self.height // 2))
         self.bottom_l_2 = (self.center_x - (self.width // 2) + self.border_rad, self.center_y - (self.height // 2))
+
+    def update_position(self, x, y):
+        self.center_x = x
+        self.center_y = y
+        self.set_points()
 
     def draw(self):
         # render the shape
