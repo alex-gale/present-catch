79953ec3484b31c9d3a4a29980894785ea60b2db
porkchop38 <porkchop3838@gmail.com>
2019-10-30 11:29:32 +0000

    Implemented rudimentary AI
    * Basic computer AI plays cards on its turn
    * Added empty play and second card piles
    * Added text labels for the play pile and previous card pile

--- present_snap.py
+++ present_snap.py
@@ -1,4 +1,4 @@
-import arcade, random
+import arcade, random, textwrap
 from classes.scene import Scene
 from classes.shapes import BorderRadiusRectangle
 
@@ -8,6 +8,8 @@ class PresentSnap(Scene):
 
         # set background colour
         arcade.set_background_color((71, 71, 71))
+        # set text colour
+        self.text_colour = (162,162,162)
 
         # initialise all the images
         self.images = arcade.load_textures("images/present_snap_sprites.png",[
@@ -42,8 +44,14 @@ class PresentSnap(Scene):
         self.computer_separator = BorderRadiusRectangle(self.game.SCREEN_WIDTH - 47, self.game.SCREEN_HEIGHT - 127, 11, 175, (25,25,25), 5)
         self.computer_decorator_card = Card(self, self.game.SCREEN_WIDTH - 105, self.game.SCREEN_HEIGHT - 127, 125, 175, colour=(240,240,240), flipped=True)
 
+        # initialise the empty play pile
+        self.empty_play_pile = BorderRadiusRectangle(345, 300, 125, 175, colour=(89,89,89), border_rad=5)
+        text_wrapper = textwrap.TextWrapper(width=15)
+        self.play_pile_desc = text_wrapper.wrap("This is the play pile.\nCards will be placed here once played.")
+
         # initialise box to show the second card
         self.second_card_box = BorderRadiusRectangle(467, 352, 50, 70, colour=(240,240,240), border_rad=5)
+        self.empty_second_box = BorderRadiusRectangle(467, 352, 50, 70, colour=(89,89,89), border_rad=5)
 
     def setup(self):
         self.score = 0
@@ -51,7 +59,7 @@ class PresentSnap(Scene):
         # runs code to add card to the button list
         self.played_card = True
 
-        self.player_turn = True
+        self.turn_number = 1
 
         # deal the cards
         self.play_pile = []
@@ -74,42 +82,71 @@ class PresentSnap(Scene):
             self.current_card.draw()
 
         # draw computer card pile
-        self.computer_pile.draw()
-        self.computer_separator.draw()
-        self.computer_decorator_card.draw()
+        if len(self.computer_hand) > 1:
+            self.computer_pile.draw()
+            self.computer_separator.draw()
+        if len(self.computer_hand) > 0:
+            self.computer_decorator_card.draw()
+
+        # tells the player which turn it is
+
+        # play pile
+        arcade.draw_text("Play Pile", 273, 353, (*self.text_colour, 140), 13, font_name="fonts/Courgette-Regular.ttf", anchor_x="center", anchor_y="center", rotation=90)
+        arcade.draw_text("Previous", 432, 353, (*self.text_colour, 140), 13, font_name="fonts/Courgette-Regular.ttf", anchor_x="center", anchor_y="center", rotation=90)
+
+        # draws the empty play pile
+        if len(self.play_pile) == 0:
+            self.empty_play_pile.draw()
+            for i, line in enumerate(self.play_pile_desc):
+                arcade.draw_text(line, 345, 365 - (20 * i), self.text_colour, 13, font_name="fonts/Courgette-Regular.ttf", anchor_x="center")
 
         # draws the top card in the play pile
-        if len(self.play_pile) != 0:
+        elif len(self.play_pile) != 0:
             self.play_pile[-1][1].draw()
 
         # displays the second card in the play pile
         if len(self.play_pile) > 1:
             self.second_card_box.draw()
             arcade.draw_rectangle_outline(467, 352, 40, 60, color=(220,32,32))
-            arcade.draw_texture_rectangle(467, 343, 40, 40, texture=self.play_pile[-2][1].image[1])
+            arcade.draw_texture_rectangle(467, 343, 40, 40, texture=self.play_pile[-2][0][1])
+        # displays empty box for the second card
+        else:
+            self.empty_second_box.draw()
 
     def update(self, delta_time):
         # when a card is played, the next card becomes the current card and becomes pressable
-        if self.played_card:
-            if len(self.player_hand) > 0 and self.player_turn:
+        if self.turn_number % 2 == 1:
+            # checks that it's the players turn
+            if len(self.player_hand) > 0 and self.played_card:
                 self.played_card = False
+                # makes the next card clickable
                 self.current_card = Card(self, 105, 127, 125, 175, image=self.player_hand[0], colour=(240,240,240), face="back")
                 self.button_list.append(self.current_card)
+        # do the computer's move if it's the computer's turn
+        elif self.turn_number % 2 == 0:
+            # self.computer_turn()
+            computer_card = Card(self, self.game.SCREEN_WIDTH - 105, self.game.SCREEN_HEIGHT - 127, 125, 175, image=self.computer_hand[0], colour=(240,240,240), flipped=True)
+            computer_card.update_position(345, 300)
+            computer_card.flipped = False
+            computer_card.face = "face"
+            self.play_pile.append([self.computer_hand.pop(0), computer_card])
+            self.turn_number += 1
 
     def play_card(self, card):
-        # moves the card to the play pile
-        card.update_position(365, 300)
-        self.play_pile.append([self.player_hand[0], self.current_card])
-
-        # updates the card to turn it and show the face
+        # moves the card to the play pile and removes from the player's hand
+        card.update_position(345, 300)
         card.face = "face"
 
-        # remove the played card from the players hand and stops it being pressable
-        self.player_hand.pop(0)
+        self.play_pile.append([self.player_hand.pop(0), self.current_card])
+        
+        # stops the played card being pressable
         self.button_list.remove(self.current_card)
 
         # updates the current card again
         self.played_card = True
+
+        # the next turn will occur (computer turn)
+        self.turn_number += 1
 
 
 class Card:
