38fc75f676194d67e8af2a23072e874894a18ee4
Alex Gale <alexgaleiii@gmail.com>
2019-11-09 21:24:13 +0000

    Added ConveyorItems
    * Added ConveyorItems which can travel across the conveyor belt at the right speed
    * Conveyor belts are now managed from one central array with all relevant data
    * Conveyor belt speeds are now randomised
    * Added 2 more conveyor belts

--- present_match.py
+++ present_match.py
@@ -1,32 +1,81 @@
-import arcade
+import arcade, random
 from classes.scene import Scene
 
 class PresentMatch(Scene):
     def __init__(self, game):
         super().__init__(game)
 
-        # initialise the conveyor belt sprite positions
-        self.conveyor_list = arcade.SpriteList()
+        # conveyor belt data storage
+        self.conveyor_belts = [
+            {"items": arcade.SpriteList(), "sprites": arcade.SpriteList(), "speed": None, "direction": 1},
+            {"items": arcade.SpriteList(), "sprites": arcade.SpriteList(), "speed": None, "direction": -1},
+            {"items": arcade.SpriteList(), "sprites": arcade.SpriteList(), "speed": None, "direction": 1}
+        ]
 
-        for x in range(0, 800, 128):
+        # insert sprites for conveyor belts
+        for x in range(0, 800, 96):
             conveyor = ConveyorBelt()
             conveyor.center_x = x
-            conveyor.center_y = 400
-            self.conveyor_list.append(conveyor)
+            conveyor.center_y = 490
+            self.conveyor_belts[0]["sprites"].append(conveyor)
+
+        for x in range(0, 800, 96):
+            conveyor = ConveyorBelt(mirrored=True)
+            conveyor.center_x = x
+            conveyor.center_y = 350
+            self.conveyor_belts[1]["sprites"].append(conveyor)
+
+        for x in range(0, 800, 96):
+            conveyor = ConveyorBelt()
+            conveyor.center_x = x
+            conveyor.center_y = 210
+            self.conveyor_belts[2]["sprites"].append(conveyor)
+
+        # initialise list of items
+        self.item_list = {"robin": "images/present_match/robin.png"}
 
     def setup(self):
-        # initialise the conveyor belt speed for the start of the game (lower is faster)
-        self.conveyor_speed = 3
-        for c in self.conveyor_list:
-            c.FRAMES_PER_UPDATE = self.conveyor_speed
+        # reset items and randomise belt speeds
+        for belt in self.conveyor_belts:
+            belt["items"] = arcade.SpriteList()
+            belt["speed"] = random.randint(2, 3)
+
+            for sprite in belt["sprites"]:
+                sprite.FRAMES_PER_UPDATE = belt["speed"]
+
+        self.add_item_to_conveyor(0)
+
+    def add_item_to_conveyor(self, conveyor_id):
+        # get the conveyor and its top y value
+        conveyor = self.conveyor_belts[conveyor_id]
+        conveyor_top_y = conveyor["sprites"][0].center_y + 16
+
+        # choose a random item from the choices
+        item_name, item_image = random.choice(list(self.item_list.items()))
+
+        # setup the item's x and y
+        item = ConveyorItem(item_image)
+        item.center_y = conveyor_top_y + (item.height / 2)
+        # choose starting side based on conveyor direction
+        item.center_x = -32 if conveyor["direction"] == 1 else 832
+
+        # add the item to the relevant conveyor belt
+        self.conveyor_belts[conveyor_id]["items"].append(item)
 
     def draw(self):
-        # draw all the conveyor belts
-        self.conveyor_list.draw()
+        # draw all the conveyor belts and their items
+        for belt in self.conveyor_belts:
+            belt["items"].draw()
+            belt["sprites"].draw()
 
     def update(self, delta_time):
-        # update animation state of conveyor belts
-        self.conveyor_list.update_animation(delta_time)
+        # update item positions and animation state in each belt
+        for belt in self.conveyor_belts:
+            for item in belt["items"]:
+                # 6 is is the number of pixels moved each frame of conveyor animation
+                item.center_x += 6 / belt["speed"] * belt["direction"]
+
+            belt["sprites"].update_animation(delta_time)
 
     def key_release(self, key, modifiers):
         if key == arcade.key.ESCAPE:
@@ -35,16 +84,16 @@ class PresentMatch(Scene):
 
 
 class ConveyorBelt(arcade.Sprite):
-    def __init__(self):
+    def __init__(self, mirrored=False):
         super().__init__()
 
         # load tilesheet of animations for conveyor
         self.animated_textures = arcade.load_textures("images/present_match/conveyor.png", [
-            [0, 0, 128,  44],
-            [128, 0, 128, 44],
-            [256, 0, 128, 44],
-            [384, 0, 128, 44]
-        ])
+            [0, 0, 96,  33],
+            [96, 0, 96, 33],
+            [192, 0, 96, 33],
+            [288, 0, 96, 33]
+        ], mirrored=mirrored)
 
         # initialise animation speed
         self.FRAMES_PER_UPDATE = 3
@@ -59,3 +108,7 @@ class ConveyorBelt(arcade.Sprite):
             self.current_texture = 0
 
         self.texture = self.animated_textures[self.current_texture // self.FRAMES_PER_UPDATE]
+
+class ConveyorItem(arcade.Sprite):
+    def __init__(self, filename=None):
+        super().__init__(filename)
